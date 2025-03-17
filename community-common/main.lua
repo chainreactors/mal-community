5deb2b66a2debda8f2d5187f0b32b4129aa86767
h3zh1 <h3zh1@qq.com>
2025-03-17 12:05:53 +0800

    feat: add kdstab

--- community-common/main.lua
+++ community-common/main.lua
@@ -3,8 +3,7 @@ some reference:
 1. https://github.com/REDMED-X/OperatorsKit
 2. https://github.com/trustedsec/CS-Situational-Awareness-BOF
 3. https://github.com/trustedsec/CS-Remote-OPs-BOF
-]]
-require("lib.lib")
+]] require("lib.lib")
 require("modules.common_bof")
 require("modules.operatorskit")
 require("modules.situationalawareness")
@@ -13,3 +12,4 @@ require("modules.recon")
 require("modules.nanodump")
 require("modules.chainreactors")
 require("modules.noconsolation")
+require("modules.kdstab")
