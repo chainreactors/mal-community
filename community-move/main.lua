acbba7c468e5f5636bbe86794dd5e30bc1c13cae
h3zh1 <h3zh1@qq.com>
2025-04-13 00:29:05 +0800

    feat: add community-move

---
+++ community-move/main.lua
@@ -0,0 +1,6 @@
+--[[
+MoveKit - Lateral Movement Tool
+Ported from: https://github.com/0xthirteen/MoveKit
+]] require("lib.lib")
+MoveKit = require("modules.movekit")
+require("modules.movedialogs")
